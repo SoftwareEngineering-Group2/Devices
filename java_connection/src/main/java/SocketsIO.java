082e0ffbce734bb3a947675b8d11e95c6dd0f87b
robin <robin.cobanov@hotmail.com>
2024-03-18 14:16:01 +0100

    Added code for receiving state of all devices on startup

--- java_connection/src/main/java/SocketsIO.java
+++ java_connection/src/main/java/SocketsIO.java
@@ -20,6 +20,11 @@ public class SocketsIO {
     private void initializeEventListeners() {
         socket.on(Socket.EVENT_CONNECT, args -> System.out.println("Connected to the server."));
 
+        //this will receive all devices status on startup
+        socket.on("all-devices", args -> {
+            System.out.println(args[0]);
+        });
+
         socket.on("device-state-changed", args -> {
             if (args.length > 0 && args[0] instanceof JSONObject) {
                 JSONObject data = (JSONObject) args[0];
@@ -33,6 +38,7 @@ public class SocketsIO {
     private void handleDeviceStateChanged(JSONObject data) {
         // this is an example of how the components can react to changes through websocket
         // later we can change the print statements for method calls.
+        System.out.println(data.toString());
         if ("whiteLed".equals(data.optString("type"))) {
             if ("off".equals(data.optString("state"))) {
                 System.out.println("whiteLed off");
