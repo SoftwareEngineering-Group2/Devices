747b3f5b91dd96fdce746d18861dc6db29226e45
robin <robin.cobanov@hotmail.com>
2024-03-18 14:26:55 +0100

    Added code to handle each device individually

--- java_connection/src/main/java/SocketsIO.java
+++ java_connection/src/main/java/SocketsIO.java
@@ -1,5 +1,7 @@
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URISyntaxException;
@@ -23,6 +25,16 @@ public class SocketsIO {
         //this will receive all devices status on startup
         socket.on("all-devices", args -> {
             System.out.println(args[0]);
+
+            try {
+                JSONArray jsonArray = (JSONArray) args[0];
+                for (int i = 0; i < jsonArray.length(); i++){
+                    System.out.println(jsonArray.get(i).toString());
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
         });
 
         socket.on("device-state-changed", args -> {
